f53d6c928e414295afdbab244b94d20f1d2343e1
Mudassir Nawaz Javaid <muhammadibraheem.dev@gmail.com>
2025-08-20 23:23:42 +0500

    swiper added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     AOS.init();
+
+    const pricingSwiper = new Swiper('.pricingSwiper', {
+        slidesPerView: 1,
+        spaceBetween: 23,
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3
+            }
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+
+    const testimSwiper = new Swiper('.testimSwiper', {
+        slidesPerView: 1,
+        spaceBetween: 23,
+        breakpoints: {
+            576: {
+                slidesPerView: 2,
+            },
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
 });
